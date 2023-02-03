81ca1e00824dee040e8cbf5b1dcd1eeebfc9b9d2
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-02-03 19:04:03 +0530

    NewFiles

---
+++ index.js
@@ -0,0 +1,38 @@
+//EXAMINE THE DOCUMENT OBJECT //
+
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.all[10]);
+// document.all[10].textContent = "Hello"
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+// console.log(document.images);
+
+//GETELEMENTBYID //
+
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = "Goodbye";
+// headerTitle.innerHTML = '<h3>Hello</h3>'
+// headerTitle.style.borderBottom = 'solid 3px #000';
+// header.style.borderBottom = 'solid 3px #000';
+
+//GETELEMENTSBYCLASSNAME  //
+
+var items = document.getElementsByClassName('title');
+console.log(items);
+items[0].textContent = "ADD ITEMS"
+items[0].style.color = "green";
+
