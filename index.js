3175ea989468a9bd3aa532b9c04a537ad11cfbcf
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-02-03 19:15:25 +0530

    changes made in index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-//EXAMINE THE DOCUMENT OBJECT //
+// EXAMINE THE DOCUMENT OBJECT //
 
-// console.dir(document);
+console.dir(document);
 
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// document.title = 123;
-// console.log(document.doctype);
-// console.log(document.head)
-// console.log(document.body)
-// console.log(document.all)
-// console.log(document.all[10]);
-// document.all[10].textContent = "Hello"
-// console.log(document.forms[0]);
-// console.log(document.links);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+document.title = 123;
+console.log(document.doctype);
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+console.log(document.all[10]);
+document.all[10].textContent = "Hello"
+console.log(document.forms[0]);
+console.log(document.links);
 
-// console.log(document.images);
+console.log(document.images);
 
-//GETELEMENTBYID //
+// GETELEMENTBYID //
 
-// console.log(document.getElementById('header-title'));
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
-// console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = "Goodbye";
-// headerTitle.innerHTML = '<h3>Hello</h3>'
-// headerTitle.style.borderBottom = 'solid 3px #000';
-// header.style.borderBottom = 'solid 3px #000';
+console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+headerTitle.innerText = "Goodbye";
+headerTitle.innerHTML = '<h3>Hello</h3>'
+headerTitle.style.borderBottom = 'solid 3px #000';
+header.style.borderBottom = 'solid 3px #000';
 
-//GETELEMENTSBYCLASSNAME  //
+// GETELEMENTSBYCLASSNAME  //
 
 var items = document.getElementsByClassName('title');
 console.log(items);
