af0b2a91799114cb82cb7aa15f73a8c67b22aab5
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-02-05 15:58:53 +0530

    event Listener

--- Task1/index.js
+++ Task1/index.js
@@ -79,95 +79,205 @@
 
 //traversing the dom
 
-var itemList = document.querySelector('#items');
+// var itemList = document.querySelector('#items');
 
-// parentNode
+// // parentNode
 
-console.log(itemList.parentNode);
-itemList.parentNode.style.backgroundColor = '#f4f4f4'
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4'
 
-// parentElement
+// // parentElement
 
-console.log(itemList.parentElement);
+// console.log(itemList.parentElement);
 
-// childNodes
+// // childNodes
 
-console.log(itemList.childNodes);
+// console.log(itemList.childNodes);
 
-console.log(itemList.children);
-console.log(itemList.children[1]);
-itemList.children[1].style.backgroundColor = 'yellow'
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow'
 
-//firstChild
+// //firstChild
 
-console.log(itemList.firstChild);
+// console.log(itemList.firstChild);
 
-//firstElementChild
+// //firstElementChild
 
-console.log(itemList.firstElementChild);
-itemList.firstElementChild.textContent = 'hello 1'
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'hello 1'
 
-//lastChild
+// //lastChild
 
-console.log(itemList.lastChild);
+// console.log(itemList.lastChild);
 
-//lastElementChild
+// //lastElementChild
 
-console.log(itemList.lastElementChild);
-itemList.lastElementChild.textContent = 'hello4'
-
-
-// nextSibling
-
-console.log(itemList.nextSibling);
-
-//nextElementSibling
-
-console.log(itemList.nextElementSibling);
-
-// previousSibling
-
-console.log(itemList.previousSibling);
-
-//previousElementSibling
-
-console.log(itemList.previousElementSibling);
-
-itemList.previousElementSibling.style.color = 'green'
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'hello4'
 
 
-//createElement
+// // nextSibling
 
-//create a div
+// console.log(itemList.nextSibling);
 
-var newDiv = document.createElement('div');
-//add class 
+// //nextElementSibling
 
-newDiv.className = 'hello'
+// console.log(itemList.nextElementSibling);
 
-//add id
+// // previousSibling
 
-newDiv.id = 'hello1'
+// console.log(itemList.previousSibling);
 
-//add attribute
+// //previousElementSibling
 
-newDiv.setAttribute('title', 'hello div');
+// console.log(itemList.previousElementSibling);
 
-//create a text node
-
-var newDivText = document.createTextNode('Hello World');
-
-//add text to div
-
-newDiv.appendChild(newDivText);
-
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+// itemList.previousElementSibling.style.color = 'green'
 
 
-console.log(newDiv)
+// //createElement
 
-newDiv.style.fontSize ='30px'
+// //create a div
+
+// var newDiv = document.createElement('div');
+// //add class 
+
+// newDiv.className = 'hello'
+
+// //add id
+
+// newDiv.id = 'hello1'
+
+// //add attribute
+
+// newDiv.setAttribute('title', 'hello div');
+
+// //create a text node
+
+// var newDivText = document.createTextNode('Hello World');
+
+// //add text to div
+
+// newDiv.appendChild(newDivText);
+
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
 
 
-container.insertBefore(newDiv, h1)
+// console.log(newDiv)
+
+// newDiv.style.fontSize ='30px'
+
+
+// container.insertBefore(newDiv, h1)
+
+// var newDiv2 = document.createElement('div')
+// newDiv2.className = 'hi'
+// newDiv2.id = 'hii'
+// newDiv2.setAttribute('title', 'hello hi');
+// var newDiv2Text = document.createTextNode('Hello World')
+
+// newDiv2.appendChild(newDiv2Text);
+// var container1 = document.querySelector('.list-group');
+// var ul = document.querySelector('li:nth-child(1)');
+
+// container1.insertBefore(newDiv2, ul)
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter')
+
+//filter event
+filter.addEventListener('keyup', filterItems);
+
+//form submit event
+form.addEventListener('submit', addItem);
+ 
+
+//delete event
+itemList.addEventListener('click', removeItem);
+
+//remove item
+function removeItem(e)
+{
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm('Are You Sure?'))
+        {
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+//Add item
+function addItem(e)
+{
+    e.preventDefault();
+
+    //get input value
+    var newItem = document.getElementById('item').value;
+
+    //create new Li element
+    var li = document.createElement('li');
+    
+    //add class
+    li.className = 'list-group-item';
+    
+    //add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    //create delete button element
+    var deleteBtn = document.createElement('button');
+
+    //create an edit button
+    var editbtn = document.createElement('button');
+
+    //add classes to delete btn
+    editbtn.className = 'btn btn-danger'
+
+    //append text node
+    editbtn.appendChild(document.createTextNode('EDIT'));
+    
+    //add classes to delete btn
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+    //append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+
+    //append button to li
+    li.appendChild(deleteBtn)
+
+    //append edit button to li
+    li.appendChild(editbtn)
+
+
+    //append li to list
+    itemList.appendChild(li);
+}
+
+
+//filter items
+function filterItems(e)
+{
+    //convert text to lowercase
+    var text = e.target.value.toLowerCase();
+
+    //Get list
+    var items = itemList.getElementsByTagName('li');
+    
+    //convert to an array
+    Array.from(items).forEach(function(item)
+    {
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1)
+        {
+            item.style.display = 'block'
+        }
+        else
+        {
+            item.style.display = 'none'
+        }
+    })
+}
